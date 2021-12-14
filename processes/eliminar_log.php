e8e0d6c8f0e3a6d21f894a8d422d21c5e08f4d0a
JuanCarlosGundin <juancarlosgr9898@gmail.com>
2021-12-14 19:48:08 +0100

    arreglo

--- processes/eliminar_log.php
+++ processes/eliminar_log.php
@@ -2,16 +2,10 @@
 
 require_once '../services/config.php';
 require_once '../services/connection.php';
-
 $id=$_POST['id'];
-
-$result = mysqli_query($conn,"DELETE FROM tbl_fecha
-WHERE id_fecha = $id");
-
 $del = $pdo->prepare("DELETE FROM tbl_fecha
 WHERE id_fecha = ?");
     $del->bindParam(1, $id);
     $del->execute();
-echo"<script>window.location.replace('../view/inicio.php')</script>";
-
+echo"<script>window.location.replace('../view/historial.php')</script>";
 ?>
